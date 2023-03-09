ed5307c0565c929ba0f5e2d0145f4263380fbdaf
Alexander Poole <alex.o.poole@gmail.com>
2023-03-09 20:06:38 +0100

    Add tailwindcss for making css managable
    close #1

--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -2,5 +2,7 @@
   import { hello } from "$lib";
 </script>
 
-<h1>Welcome to SvelteKit {hello}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="m-4">
+  <h1 class="text-3xl">Welcome to SvelteKit {hello}</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
