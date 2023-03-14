582296f58a33dabaf864963d5cf849d03e739a9d
Alexander Poole <alex.o.poole@gmail.com>
2023-03-14 11:12:03 +0100

    Add basic Navbar
    The Navbar is loosly based on:
    https://tailwindcomponents.com/component/navbar-with-tagline-and-logo
    
    TODO:
    * Test more displays
    * Add seconf link
    * Add logo image placeholder

--- packages/web/src/routes/[[lang]]/+layout.svelte
+++ packages/web/src/routes/[[lang]]/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+  import { Navbar } from "$lib/components";
   import "../../app.css";
 </script>
 
-<slot />
+<div class="flex flex-col min-h-screen">
+  <Navbar />
+  <div class="flex-1">
+    <slot />
+  </div>
+</div>
