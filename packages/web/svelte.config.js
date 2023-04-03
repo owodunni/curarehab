cecb78ed341e3d933a245ea9f129c6fe842f4a7b
Alexander Poole <alex.o.poole@gmail.com>
2023-04-03 07:09:49 +0200

    Upload sourcemaps to sentry

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 import sveltePreprocess from "svelte-preprocess";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,15 +15,7 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter()
-  },
-  plugins: [
-    sentryVitePlugin({
-      org: "alexander-poole",
-      project: "curarehab",
-      include: ".svelte-kit",
-      authToken: "413e4b21eba2442a836f76771f89b38f8c9ecdd1b28743cca018c7fbd0d1a0de"
-    })
-  ]
+  }
 };
 
 export default config;
