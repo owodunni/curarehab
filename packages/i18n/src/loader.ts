74aea16d80decfd629f5e38dd9b4d8bbf18831d4
Alexander Poole <alex.o.poole@gmail.com>
2023-05-11 06:32:15 +0200

    Move i18n implementation to seperate package
    As we migrate to skeleton ui, we want to be able to reuse the i18n code.
    
    We don't need translations to be reactive. We can load translation files on the
    server, send them over and create our helper functions for interacting
    with the files. This gives us better performance and simpler code.
    
    The end result is a better performance score since the bundle size is
    much smaller.

---
+++ packages/i18n/src/loader.ts
@@ -0,0 +1,21 @@
+import type { Config, TranslationLoader, TranslatorFactory } from "./types";
+
+export const createTranslationLoader: TranslatorFactory = <
+  Category extends string,
+  Key extends string,
+  Locale extends string
+>(
+  config: Config<Category, Locale>
+) => {
+  return {
+    loadCategories: async (categories, locale) => {
+      console.log("loadCategories", categories, locale);
+
+      return Object.fromEntries(
+        await Promise.all(
+          categories.map(async (category) => [category, await config.loader(locale, category)])
+        )
+      );
+    }
+  } satisfies TranslationLoader<Category, Key, Locale>;
+};
