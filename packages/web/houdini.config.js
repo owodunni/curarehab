ac70e481b621369aba1126f10b6929339aee1fd0
Alexander Poole <alex.o.poole@gmail.com>
2023-05-30 14:42:48 +0200

    Add houdini in order to use graphq instead of raw rest api

---
+++ packages/web/houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://jardoole.xyz/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
--- packages/web/src/routes/[[lang]]/+page.svelte
+++ packages/web/src/routes/[[lang]]/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-  import Articles from "$lib/widgets/Articles.svelte";
+  //import Articles from "$lib/widgets/Articles.svelte";
   import Hero from "$lib/widgets/Hero.svelte";
   import Terapheuts from "$lib/widgets/Terapheuts.svelte";
-  import type { PageData } from "./$types";
+  import type { PageData } from "./$houdini";
 
   export let data: PageData;
-  const { t, l, terapheuts, posts } = data;
+  const { t, l } = data;
+  console.log(data)
+
 </script>
 
 <main>
   <Hero {t} {l} />
-  <Terapheuts {terapheuts} {l} {t} />
-  <Articles {posts} {l} {t} />
+  <Terapheuts store={data.ArticlesAndTerapeuts} {l} {t} />
+  <!--Articles {posts} {l} {t} /-->
   <!--MainTreatments /-->
 </main>
 
