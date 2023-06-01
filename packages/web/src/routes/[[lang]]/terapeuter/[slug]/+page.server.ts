978f7072752a6d401c900ba20a43eaa474f5d47d
Alexander Poole <alex.o.poole@gmail.com>
2023-06-01 10:12:50 +0200

    Fix terapeut pages to work with graphql

---
+++ packages/web/src/routes/[[lang]]/terapeuter/[slug]/+page.server.ts
@@ -0,0 +1,21 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import type { TerapeutsQuery } from "./$types.gql";
+import query from "./query.gql?raw";
+
+export const load: PageServerLoad = async (event) => {
+  const data = await event.locals.client
+    .query<TerapeutsQuery>(query, {
+      filter: {
+        slug: {
+          _eq: event.params.slug
+        }
+      }
+    })
+    .toPromise();
+
+  const user = data.data?.terapeuter_directus_users.filter((u) => u.directus_users_id);
+  if (!user || user.length === 0 || !user[0].directus_users_id)
+    throw error(404, { message: "User not found" });
+  return { terapeut: user[0].directus_users_id };
+};
--- packages/web/src/routes/[[lang]]/terapeuter/[slug]/+page.svelte
+++ packages/web/src/routes/[[lang]]/terapeuter/[slug]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import Container from "$lib/components/Container.svelte";
-  import { getTitle } from "$lib/widgets/util";
+  import { getTitle, getAsset } from "$lib/widgets/util";
   import type { PageData } from "./$types";
   export let data: PageData;
-  const { lang, terapheut, t, l } = data;
+  const { t, l, terapeut } = data;
 </script>
 
 <svelte:head>
-  <title>{terapheut.first_name}</title>
-  <meta name="description" content={terapheut.profile_excerpt} />
+  <title>{terapeut.first_name}</title>
+  <meta name="description" content={terapeut.description} />
 </svelte:head>
 
 <Container class="mt-16 sm:mt-32">
@@ -16,8 +16,8 @@
     <div class="lg:pl-20">
       <div class="max-w-xs px-2.5 lg:max-w-none">
         <img
-          src={terapheut.profile_photo}
-          alt=""
+          src={getAsset(terapeut.avatar?.id)}
+          alt={terapeut.avatar?.title}
           sizes="(min-width: 1024px) 32rem, 20rem"
           class="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
         />
@@ -25,13 +25,13 @@
           <h3
             class="text-tertiary-900 group-hover:text-tertiary-600 text-lg font-semibold leading-8 tracking-tight"
           >
-            {terapheut.first_name}
-            {terapheut.last_name}
+            {terapeut.first_name}
+            {terapeut.last_name}
           </h3>
-          <p class="text-base leading-7 text-gray-600">{getTitle(terapheut.title, t)}</p>
+          <p class="text-base leading-7 text-gray-600">{getTitle(terapeut.work_title || "", t)}</p>
           <div class="mt-6 w-24">
             <a
-              href={`${l("terapeuter")}/${terapheut.first_name}-${terapheut.id}/boka`}
+              href={`${l("terapeuter")}/${terapeut.slug}/boka`}
               class="btn btn-sm variant-filled hidden sm:block"
             >
               {t("common", "bokaNu")}
@@ -42,8 +42,7 @@
     </div>
     <div class="lg:order-first lg:row-span-2">
       <article class="prose">
-        <!-- eslint-disable svelte/no-at-html-tags -->
-        {@html lang === "en" ? terapheut.profile_text_en : terapheut.profile_text}
+        {JSON.stringify(terapeut.profil_text)}
       </article>
     </div>
   </div>
