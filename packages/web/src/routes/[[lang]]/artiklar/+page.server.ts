8263cf25fa863ceccc94bc4b7278e27234085386
Alexander Poole <alex.o.poole@gmail.com>
2023-06-08 20:43:11 +0200

    Only show published articles in production

--- packages/web/src/routes/[[lang]]/artiklar/+page.server.ts
+++ packages/web/src/routes/[[lang]]/artiklar/+page.server.ts
@@ -1,3 +1,4 @@
+import { PUBLIC_RUNTIME } from "$env/static/public";
 import type { PageServerLoad } from "./$types";
 import type { ArticlesMetaDataQuery } from "./$types.gql";
 import query from "./query.gql?raw";
@@ -5,7 +6,10 @@ import query from "./query.gql?raw";
 export const load: PageServerLoad = async (event) => {
   return event.locals.client
     .query<ArticlesMetaDataQuery>(query, {
-      filter: { language: { _eq: event.params.lang ?? "sv" } }
+      filter: {
+        language: { _eq: event.params.lang ?? "sv" },
+        ...(PUBLIC_RUNTIME === "production" && { status: { _eq: "published" } })
+      }
     })
     .toPromise();
 };
