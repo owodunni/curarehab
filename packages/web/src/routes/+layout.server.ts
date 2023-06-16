a63ca3d0d585f17175fae44d20fce0943df8fae3
Alexander Poole <alex.o.poole@gmail.com>
2023-06-16 08:21:50 +0200

    Trigger 404 error from +pages and not +layout.server.ts
    This way we can still get the general layout of the site in order.
    Making it much easier to display a nice error page.

--- packages/web/src/routes/+layout.server.ts
+++ packages/web/src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 import type { LayoutServerLoad } from "./$types";
-import { error, redirect } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 import { locales, type Locale, defaultLocale, type Route, loadTranslations } from "$lib/i18n";
 import { PUBLIC_RUNTIME } from "$env/static/public";
 import query from "./query.gql?raw";
@@ -12,26 +12,35 @@ import type { WidgetsQuery } from "./$types.gql";
 export const prerender = PUBLIC_RUNTIME === "development" || PUBLIC_RUNTIME === "production";
 
 export const load: LayoutServerLoad = async (event) => {
-  const { pathname } = event.url;
-  const { lang } = event.params;
+  async function loadData(locale: Locale, route: Route) {
+    return Promise.all([
+      loadTranslations(locale, route as Route),
+      event.locals.client.query<WidgetsQuery>(query, []).toPromise()
+    ] as const);
+  }
 
-  const localeExists = locales.find((l) => l === lang);
-  if (lang && !localeExists) throw error(404, "Not found");
+  async function getLocaleAndRoute(): Promise<[Locale, Route]> {
+    const { pathname } = event.url;
+    const { lang } = event.params;
 
-  const locale: Locale = localeExists || defaultLocale;
-  const route = ((): string => {
-    if (!lang) return pathname;
-    else if (pathname === `/${lang}`) return "/";
-    else return pathname.replace(`/${lang}`, "");
-  })();
+    const localeExists = locales.find((l) => l === lang);
+    if (lang && !localeExists) return ["sv", "/"];
 
-  if (localeExists === defaultLocale) throw redirect(308, route);
+    const locale: Locale = localeExists || defaultLocale;
+    const route = ((): string => {
+      if (!lang) return pathname;
+      else if (pathname === `/${lang}`) return "/";
+      else return pathname.replace(`/${lang}`, "");
+    })();
+    if (localeExists === defaultLocale) throw redirect(308, route);
 
-  const [translations, widgets] = await Promise.all([
-    loadTranslations(locale, route as Route),
-    event.locals.client.query<WidgetsQuery>(query, []).toPromise()
-  ] as const);
+    return [locale, route as Route];
+  }
 
+  const [locale, route] = await getLocaleAndRoute();
+
+  // When navigating to a route that doesn't exist loadData can throw a No page found exception
+  const [translations, widgets] = await loadData(locale, route).catch(() => loadData(locale, "/"));
   return {
     translations,
     widgets: widgets.data
