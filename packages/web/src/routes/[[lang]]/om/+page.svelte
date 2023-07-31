cc6608de6b4f38fe9fd1c45750f234c1009e2339
Alexander Poole <alex.o.poole@gmail.com>
2023-07-31 11:20:31 +0200

    Add terapeuts to om oss page

--- packages/web/src/routes/[[lang]]/om/+page.svelte
+++ packages/web/src/routes/[[lang]]/om/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import TextPage from "$lib/components/TextPage.svelte";
+  import Section from "$lib/components/Section.svelte";
+  import Container from "$lib/components/Container.svelte";
+  import Terapheuts from "$lib/widgets/Terapheuts.svelte";
   export let data: PageData;
+  $: ({ t, l } = data);
 </script>
 
 <TextPage data={data?.data?.om} lang={data.params.lang} />
+<Section extras="theme-skog">
+  <Container>
+    <Terapheuts terapheuts={data?.data?.terapeuter_directus_users || []} {l} {t} />
+  </Container>
+</Section>
