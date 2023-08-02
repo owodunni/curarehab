f22a1ecd3eca242920ef9999a3c846af7fb98f29
Alexander Poole <alex.o.poole@gmail.com>
2023-08-02 10:50:06 +0200

    Create reusable Card and CardList components
    We want margins in cards to be similar when the cards are similar to do
    that we create reusable components.

--- packages/web/src/lib/components/TreatmentCard.svelte
+++ packages/web/src/lib/components/TreatmentCard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { Treatment } from "$lib/api";
   import type { L, T } from "$lib/i18n/t";
-  import Image from "./Image.svelte";
+  import Card from "./Card.svelte";
 
   export let treatment: Treatment;
   export let l: L;
@@ -14,43 +14,29 @@
   $: primary = treatment.Primary_treatment === "true";
 </script>
 
-<article class="flex flex-col justify-between">
-  <a href={`${l("behandlingar")}/${treatment.Slug}`} class="group">
-    <div class="relative w-full">
-      <Image
-        srcPath={treatment?.bild?.filename_disk || ""}
-        alt={treatment?.bild?.title || ""}
-        width={800}
-        height={450}
-        class={`bg-skog-400 object-cover ${
-          primary
-            ? "w-full  rounded-2xl  aspect-[16/9] sm:aspect-[2/1] lg:aspect-[3/2]"
-            : "rounded-full aspect-square h-32 w-32 mx-auto sm:h-48 sm:w-48 md:h-64 md:w-64"
-        } `}
-      />
-      <div class="absolute inset-0 rounded-2xl" />
-    </div>
-    <div class="sm:flex sm:justify-between">
-      <h3 class="text-theme-heading group-hover:text-theme-muted-hover text-lg-heading mt-6">
-        {title}
-      </h3>
+<Card
+  link={`${l("behandlingar")}/${treatment.Slug}`}
+  image={treatment?.bild}
+  text={sammanfattning}
+  shape={primary ? "square" : "circle"}
+  hideTextOnSm={!primary}
+>
+  <div>
+    <h3 class="text-theme-heading group-hover:text-theme-muted-hover text-lg-heading">
+      {title}
+    </h3>
 
-      {#if primary}
-        <a href={t("common", "hanoLink")} class="btn btn-sm variant-filled mt-6">
-          {t("common", "bokaNu")}
-        </a>
-      {/if}
-    </div>
+    {#if primary}
+      <a href={t("common", "hanoLink")} class="btn btn-sm variant-filled mt-6 sm:hidden">
+        {t("common", "bokaNu")}
+      </a>
+    {/if}
+  </div>
+  {#if primary}
     <div>
-      {#if sammanfattning}
-        <article
-          class={`text-theme-body text-base leading-7 mt-5 line-clamp-4 ${
-            primary ? "sm:line-clamp-6" : ""
-          }`}
-        >
-          {sammanfattning}
-        </article>
-      {/if}
+      <a href={t("common", "hanoLink")} class="btn btn-sm variant-filled hidden sm:block">
+        {t("common", "bokaNu")}
+      </a>
     </div>
-  </a>
-</article>
+  {/if}
+</Card>
