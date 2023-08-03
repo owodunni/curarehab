ea45efea1aca4e94ccd419ffbb200c04b1ff412b
Alexander Poole <alex.o.poole@gmail.com>
2023-08-03 13:39:44 +0200

    Add meta image to seo data
    Look here for more meta data inspiration:
    
    https://github.com/rodneylab/sveltekit-seo/tree/main/src/lib/components/SEO

--- packages/web/src/lib/components/Seo.svelte
+++ packages/web/src/lib/components/Seo.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { SeoMetaData } from "$lib/api";
+  import { getAsset2 } from "$lib/widgets/util";
 
   export let seo: SeoMetaData | null | undefined;
 </script>
@@ -8,5 +9,21 @@
   {#if seo}
     <title>{seo.title}</title>
     <meta name="description" content={seo.description} />
+    <meta property="og:title" content={seo.title} />
+    <meta property="og:description" content={seo.description} />
+    {#if seo.image && seo.image.filename_disk}
+      <meta
+        property="og:image"
+        content={getAsset2(seo.image.filename_disk, {
+          width: 1200,
+          height: 627,
+          quality: 0.8,
+          format: "png"
+        })}
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="627" />
+      <meta property="og:image:alt" content={seo.image.title} />
+    {/if}
   {/if}
 </svelte:head>
