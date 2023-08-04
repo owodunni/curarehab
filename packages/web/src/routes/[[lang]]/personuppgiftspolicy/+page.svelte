c49b5a17aa2ce42fa786e0df1ccaabdfb4692157
Alexander Poole <alex.o.poole@gmail.com>
2023-08-04 12:04:02 +0200

    Remove un implemented pages

--- packages/web/src/routes/[[lang]]/personuppgiftspolicy/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import type { PageData } from "./$types";
-  import TextPage from "$lib/components/TextPage.svelte";
-  export let data: PageData;
-</script>
-
-<TextPage data={data?.data?.sekretess} lang={data.params.lang} />
