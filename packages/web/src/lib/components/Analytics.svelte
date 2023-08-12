b8eb586b5ebd1b4f29da1ed0ea6aaddfe8ba31e6
Alexander Poole <alex.o.poole@gmail.com>
2023-08-12 12:10:27 +0200

    Add gtag for reporting analytics events

---
+++ packages/web/src/lib/components/Analytics.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { page } from "$app/stores";
+  import { PUBLIC_MEASUREMENTS_ID } from "$env/static/public";
+
+  page.subscribe((value) => console.log("page", value.route));
+</script>
