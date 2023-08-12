b59bdb2915976b81fc12a8996747ae3e9ec80be1
Alexander Poole <alex.o.poole@gmail.com>
2023-08-12 14:19:23 +0200

    Add gtag and use it without cookies
    Unitll we have a cookie dialog and policy documents in place we don't
    want to use gtag.js with cookies since that will break cookie law.

--- packages/web/src/lib/components/Analytics.svelte
+++ packages/web/src/lib/components/Analytics.svelte
@@ -1,6 +1,14 @@
-<script>
+<script lang="ts">
   import { page } from "$app/stores";
   import { PUBLIC_MEASUREMENTS_ID } from "$env/static/public";
 
-  page.subscribe((value) => console.log("page", value.route));
+  $: {
+    if (typeof gtag !== "undefined") {
+      /* global gtag */
+      gtag("config", PUBLIC_MEASUREMENTS_ID, {
+        page_title: document.title,
+        page_path: $page.url.pathname
+      });
+    }
+  }
 </script>
