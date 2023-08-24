26e9e851d4666838bd770cc7c17f18b81734c944
Alexander Poole <alex.o.poole@gmail.com>
2023-08-24 11:32:46 +0200

    Wrap cards in div to prevent weird positioning during page load

--- packages/web/src/lib/components/Card.svelte
+++ packages/web/src/lib/components/Card.svelte
@@ -8,28 +8,30 @@
   export let text: string | undefined | null = "";
 </script>
 
-<a href={link} class="group">
-  <div class="relative w-full">
-    <Image
-      srcPath={image?.filename_disk || ""}
-      alt={image?.title || ""}
-      width={shape === "square" ? 800 : 400}
-      height={shape === "square" ? 450 : 400}
-      class={`bg-skog-400 object-cover ${
-        shape === "square"
-          ? "aspect-[16/9]  w-full  rounded-2xl sm:aspect-[2/1] lg:aspect-[3/2]"
-          : "mx-auto aspect-square h-52 w-52 rounded-full md:h-64 md:w-64"
-      } `}
-    />
-    <div class="mt-6 flex items-start justify-between">
-      <slot />
+<div>
+  <a href={link} class="group">
+    <div class="relative w-full">
+      <Image
+        srcPath={image?.filename_disk || ""}
+        alt={image?.title || ""}
+        width={shape === "square" ? 800 : 400}
+        height={shape === "square" ? 450 : 400}
+        class={`bg-skog-400 object-cover ${
+          shape === "square"
+            ? "aspect-[16/9]  w-full  rounded-2xl sm:aspect-[2/1] lg:aspect-[3/2]"
+            : "mx-auto aspect-square h-52 w-52 rounded-full md:h-64 md:w-64"
+        } `}
+      />
+      <div class="mt-6 flex items-start justify-between">
+        <slot />
+      </div>
+      {#if text}
+        <article class={`prose mt-5`}>
+          <p class="text-theme-body line-clamp-6 overflow-hidden text-ellipsis text-base leading-7">
+            {text}
+          </p>
+        </article>
+      {/if}
     </div>
-    {#if text}
-      <article class={`prose mt-5`}>
-        <p class="text-theme-body line-clamp-6 overflow-hidden text-ellipsis text-base leading-7">
-          {text}
-        </p>
-      </article>
-    {/if}
-  </div>
-</a>
+  </a>
+</div>
