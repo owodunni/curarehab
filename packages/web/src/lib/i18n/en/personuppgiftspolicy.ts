117bcad1cb653d83dc7dd65404e496d731cb1e83
Alexander Poole <alex.o.poole@gmail.com>
2023-08-31 12:02:58 +0200

    Fix name of translations for personuppgiftspolicy

--- packages/web/src/lib/i18n/en/personuppgiftspolicy.ts
+++ packages/web/src/lib/i18n/en/personuppgiftspolicy.ts
@@ -1,6 +1,6 @@
 import type { PrivacyTranslations } from "../types";
 
-export const privacyPolicy: PrivacyTranslations = {
+export const personuppgiftspolicy: PrivacyTranslations = {
   title: "Privacy Policy",
   description: "Privacy Policy for CuraRehab"
 };
