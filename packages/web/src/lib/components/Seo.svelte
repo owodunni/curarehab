b6594bcbaf7f208787ef046fc3be43cb064da38b
Alexander Poole <alex.o.poole@gmail.com>
2023-09-13 13:05:03 +0200

    Add schema.org seo metadata to SEO component
    This allows us to specify more things about our articles such as author
    and publisher. We can take this further but for now we do more or less
    what medium is doing in there articles.

--- packages/web/src/lib/components/Seo.svelte
+++ packages/web/src/lib/components/Seo.svelte
@@ -1,8 +1,39 @@
 <script lang="ts">
   import type { SeoMetaData } from "$lib/api";
   import { getAsset2 } from "$lib/widgets/util";
+  import type { NewsArticle, WebPage, WithContext } from "schema-dts";
 
   export let seo: SeoMetaData | null | undefined;
+  export let ldJson: WithContext<WebPage> | WithContext<NewsArticle> | null | undefined = undefined;
+  $: if (!ldJson && seo) {
+    ldJson = {
+      "@context": "https://schema.org",
+      "@type": "WebPage",
+      ...(seo.title && { name: seo.title }),
+      ...(seo.description && { description: seo.description }),
+      ...(seo.link_photo &&
+        seo.link_photo.filename_disk && {
+          image: {
+            "@type": "ImageObject",
+            url: getAsset2(seo.link_photo.filename_disk, {
+              width: 800,
+              height: 450,
+              format: "jpg",
+              quality: 80
+            }),
+            width: "800",
+            height: "450"
+          }
+        }),
+      publisher: {
+        "@type": "Organization",
+        url: "https://curarehab.se",
+        email: "info@curarehab.se",
+        name: "CuraRehab",
+        legalName: "CuraRehab Linköping AB"
+      }
+    };
+  }
 </script>
 
 <svelte:head>
@@ -25,5 +56,9 @@
       <meta property="og:image:height" content="627" />
       <meta property="og:image:alt" content={seo.link_photo.title} />
     {/if}
+  {/if}
+  {#if ldJson}
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html `<script type="application/ld+json">${JSON.stringify(ldJson)}${"</"}script>`}
   {/if}
 </svelte:head>
--- packages/web/src/routes/[[lang]]/artiklar/[slug]/+page.svelte
+++ packages/web/src/routes/[[lang]]/artiklar/[slug]/+page.svelte
@@ -6,11 +6,67 @@
   import Image from "$lib/components/Image.svelte";
   import TerapeutCard from "$lib/components/TerapeutCard.svelte";
   import type { PageData } from "./$types";
+  import type { WithContext, NewsArticle, Person } from "schema-dts";
+  import { getAsset2 } from "$lib/widgets/util";
+
+  function userToPerson(
+    user: PageData["article"]["user_created"] | PageData["article"]["user_updated"],
+    language: PageData["article"]["language"]
+  ): [Person] | [] {
+    if (!user || !language) return [];
+    if (user.slug === "admin-user") return [];
+    return [
+      {
+        "@type": "Person",
+        name: `${user.first_name} ${user.last_name}`,
+        ...(user.email && { email: user.email }),
+        url: `https://curarehab.se/${language === "sv" ? "" : "en/"}terapeuter/${user.slug}`
+      }
+    ];
+  }
+
   export let data: PageData;
   $: ({ article } = data);
+  let ldJson: WithContext<NewsArticle> | undefined = undefined;
+  $: if (article) {
+    ldJson = {
+      "@context": "https://schema.org",
+      "@type": "NewsArticle",
+      dateCreated: article.date_created,
+      dateModified: article.date_updated,
+      datePublished: article.date_created,
+      ...(article.title && { headline: article.title, name: article.title }),
+      publisher: {
+        "@type": "Organization",
+        url: "https://curarehab.se",
+        email: "info@curarehab.se",
+        name: "CuraRehab",
+        legalName: "CuraRehab Linköping AB"
+      },
+      ...(article.sammanfattning && { description: article.sammanfattning }),
+      author: [
+        ...userToPerson(article.user_created, article.language),
+        ...(article.user_created?.slug !== article.user_updated?.slug
+          ? userToPerson(article.user_updated, article.language)
+          : [])
+      ],
+      url: `https://curarehab.se/${article.language === "sv" ? "" : "en/"}artiklar/${article.slug}`,
+      image: {
+        "@type": "ImageObject",
+        url: getAsset2(article?.omslagsbild?.filename_disk ?? "", {
+          width: 800,
+          height: 450,
+          format: "jpg",
+          quality: 80
+        }),
+        width: "800",
+        height: "450"
+      }
+    };
+  }
 </script>
 
-<Seo seo={article?.seo} />
+<Seo seo={article?.seo} {ldJson} />
 
 <Section extras="py-0 pb-20 sm:pb-32">
   <Container>
