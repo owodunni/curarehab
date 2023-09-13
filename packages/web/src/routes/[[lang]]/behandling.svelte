4c22dbe57d946971618886e709965efbd35653e5
Alexander Poole <alex.o.poole@gmail.com>
2023-09-13 21:43:35 +0200

    WIP: Add articles and behandling overview to get correct color feel
    TODO:
    Beter structure on behandlingar and text needs to be added to the cms
    system for easier updating

---
+++ packages/web/src/routes/[[lang]]/behandling.svelte
@@ -0,0 +1,69 @@
+<!--
+Ett typiskt besök hos oss: 
+Undersökning - Vid ditt första besök hos oss tar vi oss tid och lyssnar på dina besvär samt genomför en noggrann undersökning. Våra terapeuter hjälper dig förstå orsakerna och sambanden bakom dina besvär. Tillsammans skapar vi en personligt anpassad behandlingsplan som passar in i din livsstil och vardag. 
+
+Behandling - Baserat på undersökningen utför vi en behandling för att lindra smärta och återfå optimal rörelseförmåga. 
+
+
+Egenvård & hemövningar - Vi hjälper dig att hitta rätt övningar för dina besvär. Då övningarna är individuellt anpassade kan du enkelt integrera dem i din vardag för bästa möjliga resultat. 
+
+
+Uppföljning - Vi hjälper dig hela vägen, därför är regelbunden uppföljning viktig för oss. På så sätt säkerställer vi att du får den kontinuerliga vård du behöver samt ger dig bästa möjliga förutsättningar att hålla dig på rätt bana och undvika att besvären kommer tillbaka.  
+
+
+
+
+Vi hoppas att du känner dig väl omhändertagen hos oss och ser fram emot att vara en del av din resa mot bättre hälsa och välbefinnande. Tveka inte att kontakta oss om du har några frågor eller vill boka in ditt första besök. 
+
+-->
+
+<h2 class="text-theme-heading mb-8 text-center text-3xl font-semibold">
+  Hur går en behandling till?
+</h2>
+<div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+  <!-- Step 1: Undersökning -->
+  <div class="p-4">
+    <h3 class="text-theme-heading mb-4 text-xl font-semibold">1. Undersökning</h3>
+    <p class="text-theme-body">
+      Vid ditt första besök hos oss tar vi oss tid och lyssnar på dina besvär samt genomför en
+      noggrann undersökning. Vi hjälper dig förstå orsakerna och sambanden bakom dina besvär.
+      Tillsammans skapar vi en personligt anpassad behandlingsplan som passar in i din livsstil och
+      vardag.
+    </p>
+  </div>
+
+  <!-- Step 2: Behandling -->
+  <div class="p-4">
+    <h3 class="text-theme-heading mb-4 text-xl font-semibold">2. Behandling</h3>
+    <p class="text-theme-body">
+      Baserat på undersökningen utför vi en behandling för att lindra smärta och återfå optimal
+      rörelseförmåga. Vi använder olika behandlingsmetoder för att uppnå bästa möjliga resultat.
+      Allt ifrån massage, akupunktur men även stötvågsbehandling och manuella justeringar kan
+      förekomma.
+    </p>
+  </div>
+
+  <!-- Step 3: Egenvård & hemövningar -->
+  <div class="p-4">
+    <h3 class="text-theme-heading mb-4 text-xl font-semibold">3. Egenvård & hemövningar</h3>
+    <p class="text-theme-body">
+      Vi hjälper dig att hitta rätt övningar för dina besvär. Då övningarna är individuellt
+      anpassade kan du enkelt integrera dem i din vardag för bästa möjliga resultat.
+    </p>
+  </div>
+
+  <!-- Step 4: Uppföljning -->
+  <div class="p-4">
+    <h3 class="text-theme-heading mb-4 text-xl font-semibold">4. Uppföljning</h3>
+    <p class="text-theme-body">
+      Vi hjälper dig hela vägen, därför är regelbunden uppföljning viktig för oss. På så sätt
+      säkerställer vi att du får den kontinuerliga vård du behöver samt ger dig bästa möjliga
+      förutsättningar att hålla dig på rätt bana och undvika att besvären kommer tillbaka.
+    </p>
+  </div>
+</div>
+<p class="text-theme-body mx-32 mt-8 text-center">
+  Vi hoppas att du känner dig väl omhändertagen hos oss och ser fram emot att vara en del av din
+  resa mot bättre hälsa och välbefinnande. Tveka inte att kontakta oss om du har några frågor eller
+  vill boka in ditt första besök.
+</p>
