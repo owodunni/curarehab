c4bffe4eed98176ad1fd546f9217ef335c6fec2f
Alexander Poole <alex.o.poole@gmail.com>
2023-09-15 20:53:56 +0200

    Add texts to Hur en behandling går till section

--- packages/web/src/routes/[[lang]]/behandling.svelte
+++ packages/web/src/routes/[[lang]]/behandling.svelte
@@ -1,69 +1,71 @@
-<!--
-Ett typiskt besök hos oss: 
-Undersökning - Vid ditt första besök hos oss tar vi oss tid och lyssnar på dina besvär samt genomför en noggrann undersökning. Våra terapeuter hjälper dig förstå orsakerna och sambanden bakom dina besvär. Tillsammans skapar vi en personligt anpassad behandlingsplan som passar in i din livsstil och vardag. 
+<script>
+  import ArrowPath from "$lib/components/Icons/ArrowPath.svelte";
+  import Hearth from "$lib/components/Icons/Hearth.svelte";
+  import Home from "$lib/components/Icons/Home.svelte";
+  import Search from "$lib/components/Icons/Search.svelte";
+</script>
 
-Behandling - Baserat på undersökningen utför vi en behandling för att lindra smärta och återfå optimal rörelseförmåga. 
-
-
-Egenvård & hemövningar - Vi hjälper dig att hitta rätt övningar för dina besvär. Då övningarna är individuellt anpassade kan du enkelt integrera dem i din vardag för bästa möjliga resultat. 
-
-
-Uppföljning - Vi hjälper dig hela vägen, därför är regelbunden uppföljning viktig för oss. På så sätt säkerställer vi att du får den kontinuerliga vård du behöver samt ger dig bästa möjliga förutsättningar att hålla dig på rätt bana och undvika att besvären kommer tillbaka.  
-
-
-
-
-Vi hoppas att du känner dig väl omhändertagen hos oss och ser fram emot att vara en del av din resa mot bättre hälsa och välbefinnande. Tveka inte att kontakta oss om du har några frågor eller vill boka in ditt första besök. 
-
--->
-
-<h2 class="text-theme-heading mb-8 text-center text-3xl font-semibold">
-  Hur går en behandling till?
-</h2>
-<div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
-  <!-- Step 1: Undersökning -->
-  <div class="p-4">
-    <h3 class="text-theme-heading mb-4 text-xl font-semibold">1. Undersökning</h3>
-    <p class="text-theme-body">
-      Vid ditt första besök hos oss tar vi oss tid och lyssnar på dina besvär samt genomför en
-      noggrann undersökning. Vi hjälper dig förstå orsakerna och sambanden bakom dina besvär.
-      Tillsammans skapar vi en personligt anpassad behandlingsplan som passar in i din livsstil och
-      vardag.
-    </p>
-  </div>
-
-  <!-- Step 2: Behandling -->
-  <div class="p-4">
-    <h3 class="text-theme-heading mb-4 text-xl font-semibold">2. Behandling</h3>
-    <p class="text-theme-body">
-      Baserat på undersökningen utför vi en behandling för att lindra smärta och återfå optimal
-      rörelseförmåga. Vi använder olika behandlingsmetoder för att uppnå bästa möjliga resultat.
-      Allt ifrån massage, akupunktur men även stötvågsbehandling och manuella justeringar kan
-      förekomma.
-    </p>
-  </div>
-
-  <!-- Step 3: Egenvård & hemövningar -->
-  <div class="p-4">
-    <h3 class="text-theme-heading mb-4 text-xl font-semibold">3. Egenvård & hemövningar</h3>
-    <p class="text-theme-body">
-      Vi hjälper dig att hitta rätt övningar för dina besvär. Då övningarna är individuellt
-      anpassade kan du enkelt integrera dem i din vardag för bästa möjliga resultat.
-    </p>
-  </div>
-
-  <!-- Step 4: Uppföljning -->
-  <div class="p-4">
-    <h3 class="text-theme-heading mb-4 text-xl font-semibold">4. Uppföljning</h3>
-    <p class="text-theme-body">
-      Vi hjälper dig hela vägen, därför är regelbunden uppföljning viktig för oss. På så sätt
-      säkerställer vi att du får den kontinuerliga vård du behöver samt ger dig bästa möjliga
-      förutsättningar att hålla dig på rätt bana och undvika att besvären kommer tillbaka.
-    </p>
-  </div>
+<div class="mx-auto max-w-2xl lg:text-center">
+  <h2 class="text-theme-heading mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+    Hur går en behandling till?
+  </h2>
+  <p class="text-theme-body mt-6 text-lg">
+    Låt oss guida dig genom varje steg av din behandling så att du känner dig trygg inför ditt
+    besök.
+  </p>
 </div>
-<p class="text-theme-body mx-32 mt-8 text-center">
-  Vi hoppas att du känner dig väl omhändertagen hos oss och ser fram emot att vara en del av din
-  resa mot bättre hälsa och välbefinnande. Tveka inte att kontakta oss om du har några frågor eller
-  vill boka in ditt första besök.
-</p>
+<div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+  <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-4">
+    <div class="flex flex-col">
+      <dt class="text-theme-heading flex items-center gap-x-3 text-base font-semibold leading-7">
+        <Search class="text-tegel h-5 w-5 flex-none" stroke-width="1.7" />
+        Undersökning
+      </dt>
+      <dd class="text-theme-body mt-4 flex flex-auto flex-col text-base leading-7">
+        <p class="flex-auto">
+          Vid ditt första besök lyssnar vi noga på dina besvär och genomför en noggrann
+          undersökning. Tillsammans skapar vi en personligt anpassad behandlingsplan som passar in i
+          din livsstil.
+        </p>
+      </dd>
+    </div>
+    <div class="flex flex-col">
+      <dt class="text-theme-body flex items-center gap-x-3 text-base font-semibold leading-7">
+        <Hearth class="text-tegel h-5 w-5 flex-none" />
+        Behandling
+      </dt>
+      <dd class="text-theme-body mt-4 flex flex-auto flex-col text-base leading-7">
+        <p class="flex-auto">
+          Baserat på din undersökning utför vi en behandling för att lindra smärta och återfå
+          rörelseförmåga. Vi använder olika behandlingsmetoder, så som massage, akupunktur,
+          stötvågsbehandling, manuella justeringar med mera.
+        </p>
+      </dd>
+    </div>
+    <div class="flex flex-col">
+      <dt class="text-theme-heading flex items-center gap-x-3 text-base font-semibold leading-7">
+        <Home class="text-tegel h-5 w-5 flex-none" />
+        Egenvård & hemövningar
+      </dt>
+      <dd class="text-theme-body mt-4 flex flex-auto flex-col text-base leading-7">
+        <p class="flex-auto">
+          Vi hjälper dig att hitta rätt övningar och ger dig ett personligt träningsprogram som
+          passar in i din vardag och livsstil för bästa resultat.
+        </p>
+      </dd>
+    </div>
+    <div class="flex flex-col">
+      <dt class="text-theme-heading flex items-center gap-x-3 text-base font-semibold leading-7">
+        <ArrowPath class="text-tegel h-5 w-5 flex-none" />
+        Uppföljning
+      </dt>
+      <dd class="text-theme-body mt-4 flex flex-auto flex-col text-base leading-7">
+        <p class="flex-auto">
+          Regelbunden uppföljning är viktig för att säkerställa att du får den vård du behöver.
+          Tillsammans håller vi dig på rätt väg för att undvika att besvären kommer tillbaka. Vi
+          finns här för dig hela vägen.
+        </p>
+      </dd>
+    </div>
+  </dl>
+</div>
