acf232aa0a9c2238ad5334639c77893008df9a7a
Alexander Poole <alex.o.poole@gmail.com>
2023-11-11 19:42:14 +0100

    Add articles to terapeuts pages

--- packages/web/src/routes/[[lang]]/terapeuter/[slug]/+page.server.ts
+++ packages/web/src/routes/[[lang]]/terapeuter/[slug]/+page.server.ts
@@ -2,6 +2,7 @@ import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import type { TerapeutsQuery } from "./$types.gql";
 import query from "./query.gql?raw";
+import { PUBLIC_RUNTIME } from "$env/static/public";
 
 export const load: PageServerLoad = async (event) => {
   const data = await event.locals.client
@@ -10,6 +11,10 @@ export const load: PageServerLoad = async (event) => {
         slug: {
           _eq: event.params.slug
         }
+      },
+      filter2: {
+        language: { _eq: event.params.lang ?? "sv" },
+        ...(PUBLIC_RUNTIME === "production" && { status: { _eq: "published" } })
       }
     })
     .toPromise();
