d5d264898f104015d70401a6b1e2e0f052d8dcdf
Alexander Poole <alexanderp@opera.com>
2025-05-31 08:40:33 +0200

    Preselect city on the booking page

--- packages/web/src/routes/[[lang]]/boka/+page.svelte
+++ packages/web/src/routes/[[lang]]/boka/+page.svelte
@@ -39,18 +39,20 @@
   ></script>
   <!--script src="/jquery.onlinebooking.js"></script-->
   <script
-    src="https://ww1.clinicbuddy.com/onlinebooking/js/jquery.cbonlinebooking.js?v=20240327"
+    src="https://ww1.clinicbuddy.com/onlinebooking/js/jquery.cbonlinebooking.js?v=20240327&&defaults[language]=en"
   ></script>
 </svelte:head>
 
 <Section>
   <Container>
-      <h1 class="text-theme-heading text-3xl font-bold tracking-tight sm:text-4xl -mt-6">
-        {t("boka", "title")}
-      </h1>
-      <p class="text-theme-heading my-6 text-lg">
-        {t("boka", "description")}
-      </p>
+    <h1
+      class="text-theme-heading text-3xl font-bold tracking-tight sm:text-4xl -mt-6"
+    >
+      {t("boka", "title")}
+    </h1>
+    <p class="text-theme-heading my-6 text-lg">
+      {t("boka", "description")}
+    </p>
 
     {#if mount && browser}
       <div class="flex flex-grow" id="container" data-locale={locale}>
@@ -61,8 +63,13 @@
               settings: {
                 uid: "-3177",
                 embedded: true,
-                customization: { footer: { show: 0 }, header: { show: 0 } },
+                customization: {
+                  footer: { show: 0 },
+                  header: { show: 0 },
+                },
                 language,
+                defaults: { locations: [3] },
+                appearance: { filters: { activity: "false" } },
               },
             };
             $("#container").cbOnlineBooking(ob_.settings);
