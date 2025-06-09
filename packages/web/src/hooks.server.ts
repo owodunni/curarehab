4f4095f0bdea9b0821e83655eb3c4aa744bbd075
Alexander Poole <alexanderp@opera.com>
2025-06-09 20:09:46 +0200

    Remove unused sentry deps

--- packages/web/src/hooks.server.ts
+++ packages/web/src/hooks.server.ts
@@ -1,45 +1,7 @@
-import type { Handle, HandleServerError } from "@sveltejs/kit";
-import { Toucan } from "toucan-js";
-import { dev } from "$app/environment";
+import type { Handle } from "@sveltejs/kit";
 import { PUBLIC_CMS_URL, PUBLIC_MEASUREMENTS_ID } from "$env/static/public";
-import { CMS_TOKEN, SENTRY_DSN } from "$env/static/private";
+import { CMS_TOKEN } from "$env/static/private";
 import { Client, fetchExchange } from "@urql/core";
-
-const toucan = (() => {
-  let instance: Toucan | null = null;
-
-  return () => {
-    if (!instance) {
-      instance = new Toucan({ dsn: SENTRY_DSN });
-    }
-    return instance;
-  };
-})();
-
-export const handleError: HandleServerError = async ({ error, event }) => {
-  console.info("Handle error", error, event);
-
-  if (!dev) toucan().captureException(error, { data: { svelteKit: { event } } });
-
-  const toMessage = (): string => {
-    const message =
-      typeof error === "string"
-        ? error
-        : error && typeof error === "object"
-        ? "message" in error
-          ? error.message
-          : ""
-        : "";
-    try {
-      return typeof message === "string" ? message : JSON.stringify(message);
-    } catch (e) {
-      return "";
-    }
-  };
-  return {
-    message: toMessage()
-  };
-};
 
 const client = new Client({
   url: `${PUBLIC_CMS_URL}/graphql`,
@@ -62,14 +24,6 @@ export const handle: Handle = async ({ event, resolve }) => {
         html
           .replace("%lang%", event.params.lang === "en" ? "en" : "sv")
           .replaceAll("%MEASUREMENT_ID%", PUBLIC_MEASUREMENTS_ID),
-      /**
-       * There´s an issue with `filterSerializedResponseHeaders` not working when using `sequence`
-       *
-       * https://github.com/sveltejs/kit/issues/8061
-       */
-      filterSerializedResponseHeaders(name) {
-        return name === "content-range";
-      }
     }
   );
 };
