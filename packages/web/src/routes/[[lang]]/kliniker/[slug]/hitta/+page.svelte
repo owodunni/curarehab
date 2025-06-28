c640cb7b239e509cf800e922494d94d0966af2b3
Alexander Poole <alexanderp@opera.com>
2025-06-28 08:58:31 +0200

    Fix top padding on clinic pages

--- packages/web/src/routes/[[lang]]/kliniker/[slug]/hitta/+page.svelte
+++ packages/web/src/routes/[[lang]]/kliniker/[slug]/hitta/+page.svelte
@@ -2,7 +2,6 @@
   import type { Link } from "$lib/api/index.js";
   import Container from "$lib/components/Container.svelte";
   import BlocksRender from "$lib/components/EditorJs/BlocksRender.svelte";
-  import Section from "$lib/components/Section.svelte";
   import Seo from "$lib/components/Seo.svelte";
   import SocialLink from "$lib/components/SocialLink.svelte";
   import Breadcrumb from "$lib/components/Breadcrumb.svelte";
@@ -23,50 +22,48 @@
 
 <Seo seo={localized(hitta?.seo, hitta?.seo_en)} />
 
-<Section>
-  <Container>
-    <!-- Breadcrumb -->
-    <div class="mb-6">
-      <Breadcrumb
-        items={[
-          {
-            title: clinicTitle || "",
-            href: `${l("kliniker")}/${clinicSlug}`,
-          },
-          {
-            title: t("kliniker", "hittaTitle"),
-            current: true,
-          },
-        ]}
-        {l}
-        {t} />
-    </div>
+<Container class="pb-12 sm:pb-20">
+  <!-- Breadcrumb -->
+  <div class="mb-6">
+    <Breadcrumb
+      items={[
+        {
+          title: clinicTitle || "",
+          href: `${l("kliniker")}/${clinicSlug}`,
+        },
+        {
+          title: t("kliniker", "hittaTitle"),
+          current: true,
+        },
+      ]}
+      {l}
+      {t} />
+  </div>
 
-    <div
-      class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-      <div class="lg:pt-20">
-        <iframe
-          class="h-80 w-full sm:h-96 lg:h-[424px]"
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-          src={hitta.location?.link}
-          title="map"></iframe>
-        <ul class="border-1 mt-8 flex flex-col space-y-4 border-t pt-8">
-          {#each hitta?.links || [] as link (link?.links_id?.link)}
-            {#if link?.links_id}
-              <SocialLink
-                class="text-skog-700 hover:text-skog-900 flex gap-x-4 text-sm leading-6"
-                link={link.links_id as Link}
-                onlyIcon={false} />
-            {/if}
-          {/each}
-        </ul>
-      </div>
-      <div>
-        <div class="prose text-theme-body text-base leading-7">
-          <BlocksRender blocks={localized(hitta.text, hitta.text_en)?.blocks || []} />
-        </div>
+  <div
+    class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+    <div class="lg:pt-20">
+      <iframe
+        class="h-80 w-full sm:h-96 lg:h-[424px]"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+        src={hitta.location?.link}
+        title="map"></iframe>
+      <ul class="border-1 mt-8 flex flex-col space-y-4 border-t pt-8">
+        {#each hitta?.links || [] as link (link?.links_id?.link)}
+          {#if link?.links_id}
+            <SocialLink
+              class="text-skog-700 hover:text-skog-900 flex gap-x-4 text-sm leading-6"
+              link={link.links_id as Link}
+              onlyIcon={false} />
+          {/if}
+        {/each}
+      </ul>
+    </div>
+    <div>
+      <div class="prose text-theme-body text-base leading-7">
+        <BlocksRender blocks={localized(hitta.text, hitta.text_en)?.blocks || []} />
       </div>
     </div>
-  </Container>
-</Section>
+  </div>
+</Container>
--- packages/web/src/routes/[[lang]]/kliniker/[slug]/om/+page.svelte
+++ packages/web/src/routes/[[lang]]/kliniker/[slug]/om/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import type { PageData } from "./$types";
 
-  import Section from "$lib/components/Section.svelte";
   import Container from "$lib/components/Container.svelte";
   import Image from "$lib/components/Image.svelte";
   import Seo from "$lib/components/Seo.svelte";
@@ -31,49 +30,47 @@
   <Seo seo={lang === "en" ? om.seo_en : om.seo} />
 
   <!-- Breadcrumb -->
-  <Container>
-    <Section>
-      <div class="mb-6">
-        <Breadcrumb
-          items={[
-            {
-              title: clinicTitle,
-              href: `${l("kliniker")}/${data.params.slug}`,
-            },
-            {
-              title: t("kliniker", "omTitle"),
-              current: true,
-            },
-          ]}
-          {l}
-          {t} />
-      </div>
+  <Container class="pb-12 sm:pb-20">
+    <div class="mb-6">
+      <Breadcrumb
+        items={[
+          {
+            title: clinicTitle,
+            href: `${l("kliniker")}/${data.params.slug}`,
+          },
+          {
+            title: t("kliniker", "omTitle"),
+            current: true,
+          },
+        ]}
+        {l}
+        {t} />
+    </div>
 
-      <div
-        class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-        <div class="mx-auto lg:pt-20">
-          <div class="lg:max-w-lg">
-            {#if om?.omslagsbild}
-              <Image
-                class="h-full w-full rounded-2xl object-cover"
-                alt={om.omslagsbild?.title || ""}
-                height={800}
-                srcPath={om.omslagsbild?.filename_disk || ""}
-                width={800} />
-            {/if}
-          </div>
-        </div>
-        <div>
-          <div
-            class="prose text-theme-body hyphens-auto text-base leading-7"
-            lang={t("common", "lang")}>
-            <BlocksRender blocks={(lang === "en" ? om.text_en : om.text)?.blocks || []} />
-          </div>
-          {#if users.length > 0}
-            <ImageList images={users} />
+    <div
+      class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      <div class="mx-auto lg:pt-20">
+        <div class="lg:max-w-lg">
+          {#if om?.omslagsbild}
+            <Image
+              class="h-full w-full rounded-2xl object-cover"
+              alt={om.omslagsbild?.title || ""}
+              height={800}
+              srcPath={om.omslagsbild?.filename_disk || ""}
+              width={800} />
           {/if}
         </div>
       </div>
-    </Section>
+      <div>
+        <div
+          class="prose text-theme-body hyphens-auto text-base leading-7"
+          lang={t("common", "lang")}>
+          <BlocksRender blocks={(lang === "en" ? om.text_en : om.text)?.blocks || []} />
+        </div>
+        {#if users.length > 0}
+          <ImageList images={users} />
+        {/if}
+      </div>
+    </div>
   </Container>
 {/if}
